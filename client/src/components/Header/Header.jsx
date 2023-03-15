a2ef6d8695239c00194195c896ba24e86cf7a554
pri65 <priyankasoniii65@gmail.com>
2023-03-15 13:02:26 +0530

    Setting up routes

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import { useEffect, useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { TbSearch } from "react-icons/tb";
+import { CgShoppingCart } from "react-icons/cg";
+import { AiOutlineHeart } from "react-icons/ai";
+import Search from "./Search/Search";
+import { Context } from "../../utils/context";
+import Cart from "../Cart/Cart";
+
+import "./Header.scss";
+const Header = () => {
+    return <div>Header</div>;
+};
+
+export default Header;
